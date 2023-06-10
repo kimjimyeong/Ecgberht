2f3998e063fbe0f38a0064e83083097516779650
KimJmNoteBook <ehxhfl1589@gmail.com>
2023-06-10 17:17:28 +0900

    * Purpose: 유닛들의 중심좌표 업데이트 정확도 유무 확인 * Input: updateCentroid Set<UnitInfo>          Position(10,10), Position(10,10), Position(10,10) * Expected:10, 10 * return true */

---
+++ src/test/java/ecgberht/Clustering/ClusterTest.java
@@ -0,0 +1,57 @@
+package ecgberht.Clustering;
+
+import ecgberht.UnitInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openbw.bwapi4j.Position;
+import org.openbw.bwapi4j.unit.PlayerUnit;
+
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ClusterTest {
+
+    private Cluster cluster;
+    private UnitInfo unitInfo;
+
+    @BeforeEach
+    public void setup(){
+        cluster = new Cluster();
+
+        PlayerUnit unit = mock(PlayerUnit.class);
+        when(unit.getPosition()).thenReturn(new Position(10, 10));
+        unitInfo = new UnitInfo(unit);
+        unitInfo.visible = true;
+    }
+
+    @DisplayName("유닛들간의 중심좌표 업데이트")
+    @Test
+    public void testUpdateCentroid(){
+        PlayerUnit unit1 = mock(PlayerUnit.class);
+        when(unit1.getPosition()).thenReturn(new Position(10, 10));
+        UnitInfo unitInfo1 = new UnitInfo(unit1);
+        unitInfo1.visible = true;
+
+        PlayerUnit unit2 = mock(PlayerUnit.class);
+        when(unit2.getPosition()).thenReturn(new Position(10, 10));
+        UnitInfo unitInfo2 = new UnitInfo(unit2);
+        unitInfo2.visible = true;
+
+        cluster.units.add(unitInfo);
+        cluster.units.add(unitInfo1);
+        cluster.units.add(unitInfo2);
+
+        double expectedX = 10;
+        double expectedY = 10;
+
+        cluster.updateCentroid();
+
+        Assertions.assertEquals(expectedX, cluster.modeX);
+        Assertions.assertEquals(expectedY, cluster.modeY);
+
+    }
+
+}
