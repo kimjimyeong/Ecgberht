8419be9ac88a3bb8db7c47a392aa5eb54431cbee
KimJmNoteBook <ehxhfl1589@gmail.com>
2023-06-10 20:05:30 +0900

    * Purpose: Cluster CMaxDist Update Units * Input: updateCMaxDistFromCenter Set<UnitInfo>          Position(10,10) * 3 * Expected:13.0 * return true */

--- src/test/java/ecgberht/Clustering/ClusterTest.java
+++ src/test/java/ecgberht/Clustering/ClusterTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openbw.bwapi4j.Position;
+import org.openbw.bwapi4j.unit.Dropship;
 import org.openbw.bwapi4j.unit.PlayerUnit;
+import org.openbw.bwapi4j.unit.Unit;
 
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -26,11 +33,50 @@ class ClusterTest {
         when(unit.getPosition()).thenReturn(new Position(10, 10));
         unitInfo = new UnitInfo(unit);
         unitInfo.visible = true;
+        cluster.units.add(unitInfo);
     }
 
     @DisplayName("유닛들간의 중심좌표 업데이트")
     @Test
     public void testUpdateCentroid(){
+        PlayerUnit unit1 = mock(PlayerUnit.class);
+
+        when(unit1.getPosition()).thenReturn(new Position(20, 20));
+        UnitInfo unitInfo1 = new UnitInfo(unit1);
+        unitInfo1.visible = true;
+
+
+        PlayerUnit unit2 = mock(PlayerUnit.class);
+        when(unit2.getPosition()).thenReturn(new Position(10, 10));
+        UnitInfo unitInfo2 = new UnitInfo(unit2);
+        unitInfo2.visible = true;
+
+        cluster.units.add(unitInfo1);
+        cluster.units.add(unitInfo2);
+
+
+        double expectedX = 13.333;
+        double expectedY = 13.333;
+
+        cluster.updateCentroid();
+
+        assertEquals(expectedX, cluster.modeX, 0.001);
+        assertEquals(expectedY, cluster.modeY, 0.001);
+
+    }
+
+    @DisplayName("유닛이 하나일 떄 중심좌표로부터 CMaxDist 구하기 ")
+    @Test
+    void testUpdateCMaxDistFromCenterOneUnit(){
+        double expectedMaxDistFromCenter = 0;
+        cluster.updateCMaxDistFromCenter();
+
+        assertEquals(expectedMaxDistFromCenter, cluster.maxDistFromCenter);
+    }
+
+    @DisplayName("유닛이 여러개일 때 중심좌표로부터 CMaxDist 구하기")
+    @Test
+    void testUpdateCMaxDistFromCenterUnits(){
         PlayerUnit unit1 = mock(PlayerUnit.class);
         when(unit1.getPosition()).thenReturn(new Position(10, 10));
         UnitInfo unitInfo1 = new UnitInfo(unit1);
@@ -41,25 +87,12 @@ class ClusterTest {
         UnitInfo unitInfo2 = new UnitInfo(unit2);
         unitInfo2.visible = true;
 
-        cluster.units.add(unitInfo);
         cluster.units.add(unitInfo1);
         cluster.units.add(unitInfo2);
 
-        double expectedX = 10;
-        double expectedY = 10;
-
-        cluster.updateCentroid();
-
-        assertEquals(expectedX, cluster.modeX);
-        assertEquals(expectedY, cluster.modeY);
-
-    }
-
-    @DisplayName("중심좌표로부터 CMaxDist 구하기")
-    @Test
-    void testUpdateCMaxDistFromCenterOneUnit(){
-        double expectedMaxDistFromCenter = 0;
         cluster.updateCMaxDistFromCenter();
+
+        double expectedMaxDistFromCenter = 13.0;
 
         assertEquals(expectedMaxDistFromCenter, cluster.maxDistFromCenter);
     }
