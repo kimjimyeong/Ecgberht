55ff169bc3f01349cad9912aec3daeccada35da6
kimjimyeong <81976170+kimjimyeong@users.noreply.github.com>
2023-06-11 01:08:27 +0900

    Test and dependency (#32)
    * * Purpose: 거리 계산 함수들의 정확한 거리 계산 유무 확인
    * Input: getDistance Position(20, 20),(40, 40)
    * Expected:13, 40
    * return true
    */
    
    * Input: getDistance PlayerUnit(Position (20, 20), (40, 40))
    * Expected:13, 40
    * return true
    */
    
    * Input: getDistance UnitInfo(PlayerUnit Position(20, 20))
    * Expected:13
    * return true
    */
    
    * Input: getPredictedDistance UnitInfo(PlayerUnit Position(20, 20), VelocityX 5, VelocityY 5, )
    * Expected:20
    * return true
    
    * Input: getPredictedDistance UnitInfo(PlayerUnit Position(20, 20), VelocityX 5, VelocityY 5, ), frame 2
    * Expected:26
    * return true
    */
    
    * 의존성 변경
    
    * 의존성 변경

---
+++ test/java/ecgberht/UnitInfoDistanceTest.java
@@ -0,0 +1,117 @@
+package ecgberht;
+
+
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import org.junit.jupiter.api.Test;
+import org.openbw.bwapi4j.Position;
+import org.openbw.bwapi4j.unit.PlayerUnit;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class UnitInfoDistanceTest {
+
+    private UnitInfo unitInfo;
+    private UnitInfoDistance unitInfoDistance;
+
+    @BeforeEach
+    void setUp() {
+        PlayerUnit unit = mock(PlayerUnit.class);
+        when(unit.getPosition()).thenReturn(new Position(10, 10));
+
+        unitInfo = new UnitInfo(unit);
+        unitInfoDistance = new UnitInfoDistance(unitInfo);
+    }
+
+
+
+
+    @Test
+    void testGetDistanceUnitAndDiStance(){
+        int expectedDistance = 13;
+        int expectedDistance2 = 40;
+        assertEquals(expectedDistance, unitInfoDistance.getDistance(new Position(20, 20)));;
+        assertEquals(expectedDistance2, unitInfoDistance.getDistance(new Position(40, 40)));
+    }
+
+
+
+
+
+    @Test
+    void testGetDistanceUnitAndUnit(){
+        PlayerUnit unit1 = mock(PlayerUnit.class);
+        when(unit1.getPosition()).thenReturn(new Position(20, 20));
+
+        PlayerUnit unit2 = mock(PlayerUnit.class);
+        when(unit2.getPosition()).thenReturn(new Position(40, 40));
+
+        int expectedDistance = 13;
+        int expectedDistance2 = 40;
+        assertEquals(expectedDistance, unitInfoDistance.getDistance(unit1));
+        assertEquals(expectedDistance2, unitInfoDistance.getDistance(unit2));
+    }
+
+
+    @Test
+    void testGetDistanceUnitAndUnitInfo(){
+        PlayerUnit target = mock(PlayerUnit.class);
+        when(target.getPosition()).thenReturn(new Position(20, 20));
+
+        UnitInfo targetInfo = new UnitInfo(target);
+
+        int expectedDistance1 = 13;
+        assertEquals(expectedDistance1, unitInfoDistance.getDistance(targetInfo));
+
+        targetInfo.visible = true;
+
+        int expectedDistance2 = 0;
+        assertEquals(expectedDistance2, unitInfoDistance.getDistance(targetInfo));
+    }
+
+
+    @Test
+    void testGetPredictedDistanceUnitAndUnitInfoWithoutFrame(){
+        PlayerUnit target = mock(PlayerUnit.class);
+        when(target.getPosition()).thenReturn(new Position(20, 20));
+        when(target.getVelocityX()).thenReturn(5.0);
+        when(target.getVelocityY()).thenReturn(5.0);
+
+        UnitInfo targetInfo = new UnitInfo(target);
+        targetInfo.speed = 1.0;
+
+        int expectedDistance1 = 20;
+        assertEquals(expectedDistance1, unitInfoDistance.getPredictedDistance(targetInfo));
+    }
+
+    @Test
+    void testGetPredictedDistanceUnitAndUnitInfoWithFrame(){
+        PlayerUnit target = mock(PlayerUnit.class);
+        when(target.getPosition()).thenReturn(new Position(20, 20));
+        when(target.getVelocityX()).thenReturn(5.0);
+        when(target.getVelocityY()).thenReturn(5.0);
+
+        UnitInfo targetInfo = new UnitInfo(target);
+        targetInfo.speed = 1.0;
+
+        int frame = 2;
+        int expectedDistance1 = 26;
+        assertEquals(expectedDistance1, unitInfoDistance.getPredictedDistance(targetInfo, frame));
+    }
+
+}
